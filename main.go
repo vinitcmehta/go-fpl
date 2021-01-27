56a9f66f0f75c8f5c793a10e72519000a4cdace3
vinitcmehta <vinit_m@hotmail.co.uk>
2021-01-27 20:39:09 +0000

    adding http get

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	res, err := http.Get("https://jsonplaceholder.typicode.com/users/1")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, _ := ioutil.ReadAll(res.Body)
+
+	res.Body.Close()
+
+	fmt.Printf("%s\n", data)
 }
