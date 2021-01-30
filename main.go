8494510fe5b017e58e2b4066a5a7e5be19714382
vinitcmehta <vinit_m@hotmail.co.uk>
2021-01-30 15:58:35 +0000

    further comments

--- main.go
+++ main.go
@@ -11,7 +11,14 @@ func main() {
 	fmt.Println("Hello world!")
 
 	//TODO convert json response of api into golang object
+	//TODO inspect network tab to find APIs
+	//TODO find top in form players based off last 3 weeks who aren't picked
+	//TODO authenticate to my team
 	//TODO get list of draft apis
+	//https://draft.premierleague.com/api/bootstrap-static     players info
+	//https://draft.premierleague.com/api/bootstrap-dynamic    user specific details of draft league
+	//https://draft.premierleague.com/api/draft/44783/choices    drafted players in a league with given ID
+
 	res, err := http.Get("https://draft.premierleague.com/api/bootstrap-static")
 
 	if err != nil {
